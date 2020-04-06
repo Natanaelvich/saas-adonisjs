4dd60b2da721e885c686be9e1f74bbddba097c26
natanaelvich <taelima1997@gmail.com>
2020-04-06 15:12:52 -0300

    created controller members

--- start/routes.js
+++ start/routes.js
@@ -38,4 +38,7 @@ Route.group(() => {
     .middleware(
       new Map([[['projects.store', 'projects.update'], ['can:projects_create']]])
     )
+
+  Route.get('members', 'MemberController.index')
+  Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
 }).middleware(['auth', 'team'])
