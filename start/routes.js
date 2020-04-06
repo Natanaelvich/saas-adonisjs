10d9bfa5d34608fe44525e7052c22fdad398df0d
natanaelvich <taelima1997@gmail.com>
2020-04-06 15:24:17 -0300

    created permissions controller

--- start/routes.js
+++ start/routes.js
@@ -41,4 +41,6 @@ Route.group(() => {
 
   Route.get('members', 'MemberController.index')
   Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
+
+  Route.get('permissions', 'PermissionController.show')
 }).middleware(['auth', 'team'])
